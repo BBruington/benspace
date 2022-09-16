b271fd64e7f2d15f3a4bea67aa041417922dbbd3
BBruington <Psychological_Chemist@hotmail.com>
2022-09-16 16:49:38 -0400

    markdown

--- apps/site/pages/articles/[slug].tsx
+++ apps/site/pages/articles/[slug].tsx
@@ -1,9 +1,11 @@
-import { GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 import fs from 'fs';
 import { join } from 'path';
 import styles from './articles.module.css';
-
+import {
+  getParsedFileContentBySlug
+} from '@benspace/markdown'
 
 interface ArticleProps extends ParsedUrlQuery {
   slug: string;
@@ -11,7 +13,23 @@ interface ArticleProps extends ParsedUrlQuery {
 
 const POSTS_PATH = join(process.cwd(), '_articles');
 
+export const getStaticProps: GetStaticProps<ArticleProps> = async ({
+  params,
+}: {
+  params: ArticleProps;
+}) => {
+  // read markdown file into content and frontmatter
+  const articleMarkdownContent = getParsedFileContentBySlug(
+    params.slug,
+    POSTS_PATH
+  );
 
+  return {
+    props: {
+      slug: params.slug,
+    },
+  };
+};
 
 export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
   const paths = fs
--- libs/markdown/src/lib/markdown.ts
+++ libs/markdown/src/lib/markdown.ts
@@ -1,3 +1,25 @@
+import { MarkdownDocument } from './types';
+import fs from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
+
+
 export function markdown(): string {
   return 'markdown';
 }
+
+export const getParsedFileContentBySlug = (
+  slug: string,
+  postsPath: string
+): MarkdownDocument => {
+
+  const postFilePath = join(postsPath, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(postFilePath);
+
+const { data, content } = matter(fileContents);
+
+  return {
+    frontMatter: data,
+    content,
+  };
+};
