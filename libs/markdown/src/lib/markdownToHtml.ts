331332fc9835c39de47d1acb4bb0f68d40ee84c7
BBruington <Psychological_Chemist@hotmail.com>
2022-09-18 19:31:23 -0400

    tsconfig

--- libs/markdown/src/lib/markdownToHtml.ts
+++ libs/markdown/src/lib/markdownToHtml.ts
@@ -2,6 +2,8 @@ import remark from 'remark';
 import html from 'remark-html';
 
 export async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+  .use(html).process(markdown);
+  console.log(result);
   return result.toString();
 }
